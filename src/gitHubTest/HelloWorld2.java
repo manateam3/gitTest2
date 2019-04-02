abe7c92364ee92e60d4b2a9b5a9aa6556999fdea
manateam3 <manateam2@gmail.com>
2019-04-02 22:42:06 +0900

    Hi 2 update...

--- src/gitHubTest/HelloWorld2.java
+++ src/gitHubTest/HelloWorld2.java
@@ -5,6 +5,7 @@ public class HelloWorld2 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("Hi world222!");
+		System.out.println("Hi world223!");
 	}
 
 }
