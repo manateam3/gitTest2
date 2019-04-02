9a0010ab8bd01937a1bab336accc86131d5ac5aa
manateam3 <manateam2@gmail.com>
2019-04-02 22:52:58 +0900

    hi 3-1 update 3-2

--- src/gitHubTest/HelloWorld3.java
+++ src/gitHubTest/HelloWorld3.java
@@ -5,7 +5,7 @@ public class HelloWorld3 {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		System.out.println("Hello world 3");
-		System.out.println("Hello world 3-1");
+		System.out.println("Hello world 3-2");
 	}
 
 }
